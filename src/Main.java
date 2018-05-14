b3d01801f9237dc0d975c03a0a9d5eafbc56f9e7
p-frere <phoebefrere@gmail.com>
2018-05-14 22:25:58 +0100

    project upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *  Comp1206 CourseWork
+ *  This is the Main class where the program window is created
+ *  @author Phoebe Frere
+ */
+public class Main {
+    public static void main(String[] args) {
+        new UI().init();
+
+    }
+}
